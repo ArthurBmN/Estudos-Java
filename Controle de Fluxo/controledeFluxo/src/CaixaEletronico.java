dc762d2260ff101046a958b7aeeb8069c28cd41a
Arthur <arthurmelonunesbernardo@gmail.com>
2024-01-25 19:33:49 -0300

    Estudos  dia 25

--- Controle de Fluxo/controledeFluxo/src/CaixaEletronico.java
+++ Controle de Fluxo/controledeFluxo/src/CaixaEletronico.java
@@ -1,12 +1,16 @@
-public class CaixaEletronico {
+public class caixaEletronico {
     public static void main(String[] args) {
 
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 10.0;
  
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
          saldo = saldo - valorSolicitado;
- 
+         System.out.println("Saldo Disponível!");
          System.out.println(saldo);
+        }else{
+        System.out.println("Saldo Insuficiente!");
+        System.out.println(saldo);
+    }
 }
 }
